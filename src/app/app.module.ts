41c47d76f04afdd5592f3e235e84ffb344d0d39b
noreengul <noreen.projectmarker@nxvt.com>
2022-12-23 12:07:00 +0500

    changes for the day 21 december 2022

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,13 @@ import { LocationsComponent } from './locations/locations.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MenuComponent } from './menu/menu.component';
 import { AccountsComponent } from './accounts/accounts.component';
- import { AddLocationComponent } from './locations/add-location/add-location.component';
+import { AddLocationComponent } from './locations/add-location/add-location.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import {FormsModule} from "@angular/forms";
-import {LocationService} from "./locations/location.service";
-import {GroupService} from "./shared/group.service";
+import { FormsModule } from "@angular/forms";
+import { LocationService } from "./locations/location.service";
+import { GroupService } from "./shared/group.service";
 import { EditLocationComponent } from './locations/edit-location/edit-location.component';
-import { HttpClientModule} from "@angular/common/http";
+import { HttpClientModule } from "@angular/common/http";
 import { PathLocationComponent } from './locations/edit-location/path-location/path-location.component';
 
 @NgModule({
